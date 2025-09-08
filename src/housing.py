dd27f54ef8c8fabb898de880ed66552a8446e0bd
theraSara <s.saeedalhajeri@gmail.com>
2025-09-08 19:41:03 +0400

    Resolve merge conflict in housing.py

--- src/housing.py
+++ src/housing.py
@@ -18,17 +18,14 @@ BASE_PIPELINE = [
 ]
 
 LINEAR_GRID = {
-    "model__alpha": [0.001, 0.01, 0.1, 1.0, 10.0],
+    "model__alpha": [0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 1.0, 10.0],
     "model__l1_ratio": [0.0, 0.5, 1.0],
 }
 
 MODELS = {
     "simple_elastic": {
         "pipeline": Pipeline(BASE_PIPELINE + [("model", ElasticNet(max_iter=1000))]),
-        "param_grid": {
-            "model__alpha": [0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 1.0, 10.0],
-            "model__l1_ratio": [0.0, 0.5, 1.0],
-        },
+        "param_grid": LINEAR_GRID,
     },
     "poly_elastic_3": {
         "pipeline": Pipeline(
@@ -48,10 +45,7 @@ MODELS = {
                 ("model", ElasticNet(max_iter=1000)),
             ]
         ),
-        "param_grid": {
-            "model__alpha": [0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 1.0, 10.0],
-            "model__l1_ratio": [0.0, 0.5, 1.0],
-        },
+        "param_grid": LINEAR_GRID,
     },
     "knn": {
         "pipeline": Pipeline(BASE_PIPELINE + [("model", KNeighborsRegressor())]),
